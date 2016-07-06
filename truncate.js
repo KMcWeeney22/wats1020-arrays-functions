11e3d5e2a98b883a8cea8027e2c8911f990e14a5
mcweeney <mcweeney@seattleu.edu>
2016-07-05 21:45:11 -0400

    working toward stretch goals

--- truncate.js
+++ truncate.js
@@ -19,10 +19,7 @@
 
 
 "use strict";
-// original text that were using truncation on
-var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
-// the maximum amount of words we want for our truncation
-var wordLimit = 8;
+
 // function taking two arguments(original text and the total words in that text) and turning it into an array
 // also using that array to find the number of words to remove to create the truncated text(shortText)
 var truncateWords = function(longText, numWords){
@@ -33,14 +30,18 @@ var truncateWords = function(longText, numWords){
     // number or words - word limit = how many words need to be removed
     var remove = numWords - wordLimit;
     // word limit = where to start on string. then remove variable will tell how many words to remove and then adding "..." to the end
-    var shortened = originalTextArray.splice(wordLimit, (remove), '...');
+    originalTextArray.splice(wordLimit, (remove), '...');
     // joing everything backtogether to create the short version on the original string.
-    var shortText = originalTextArray.join(' ');   
+    var finalText = originalTextArray.join(' ');   
     console.log("Number of words in original text: " + numWords);
     console.log("Number of Words to remove: " + remove);
+    return finalText
+}
+// original text that were using truncation on
+var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
+// the maximum amount of words we want for our truncation
+var wordLimit = 8;
+var shortText = truncateWords(originalText, wordLimit)
     console.log('originalText: ' + originalText);
     console.log('shortText: ' + shortText);
-
-}
-var shortText = truncateWords(originalText, wordLimit)
 
